86d21f8be5fc22956cf5742fb916f885625226cb
clinton5n <tinnorman@gmail.com>
2015-12-07 12:29:07 +1300

    changed card: to source:

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -12,7 +12,7 @@ class Payment < ActiveRecord::Base
 	end
 
 	def process_payment
-		customer = Stripe::Customer.create email: email, card: token
+		customer = Stripe::Customer.create email: email, source: token
 		Stripe::Charge.create customer: customer.id,
 													amount: 1000,
 													description: 'Premium',
