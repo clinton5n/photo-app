9fa3b7aef2899dd27f54c20977b24bba972ff352
clinton5n <tinnorman@gmail.com>
2015-12-07 12:54:55 +1300

    debugging card payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -12,11 +12,15 @@ class Payment < ActiveRecord::Base
 	end
 
 	def process_payment
-		customer = Stripe::Customer.create email: email, source: token
+		customer = Stripe::Customer.create email: email 
+
 		Stripe::Charge.create customer: customer.id,
 													amount: 1000,
 													description: 'Premium',
-													currency: 'usd'
+													currency: 'usd',
+													card: token
 	end
 
 end
+
+
